be8694742bd724519e161149698b174da1b82676
Amin Beterbekow <betteramsly>
2022-06-12 16:37:51 +0300

    commit-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import './scss/app.scss'
+
+import Header from './components/Header'
+import Categories from './components/Categories'
+import Sort from './components/Sort'
+import PizzaBlock from './components/PizzaBlock'
+import pizzas from './assets/pizza.json'
 
 function App() {
+
+  fetch('https://62a362e25bd3609cee693e4f.mockapi.io/items')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {pizzas.map((pizza) => (
+              <PizzaBlock key={pizza.id} {...pizza}/>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Categories.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0)
+
+  const categories = [
+    'Все',
+    'Мясные',
+    'Вегетарианская',
+    'Гриль',
+    'Острые', 
+    'Закрытые',
+  ]
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index)
+  }
+
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, index) => (
+          <li
+            key={index} 
+            onClick={() => onClickCategory(index)}
+            className={activeIndex === index ? 'active' : ''}
+          >
+            {value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Categories
