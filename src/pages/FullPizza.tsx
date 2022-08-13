972763aa38f09d5962427ac33305f2121e31208f
Amin Beterbekow <betteramsly>
2022-08-13 15:57:57 +0300

    исправил адаптив

--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -33,9 +33,9 @@ const FullPizza: React.FC = () => {
 
   return (
     <div className="container">
-      <img src={pizza.imageUrl} />
+      <img className='imagePizza' src={pizza.imageUrl} />
       <h2>{pizza.title}</h2>
-      <h4>{pizza.price} ₽</h4>
+      <h4 className='priceStyle'>{pizza.price} ₽</h4>
       <Link to="/">
         <button className="button button--outline button--add">
           <span>Назад</span>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import qs from 'qs';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 import { Categories, Sort, PizzaBlock, Skeleton, Pagination } from '../components';
-
-import { sortList } from '../components/Sort';
 
 import { useAppDispatch } from '../redux/store';
 import { selectFilter } from '../redux/filter/selectors';
@@ -51,49 +48,8 @@ const Home: React.FC = () => {
 
   // Если изменили параметры и был первый рендер
   React.useEffect(() => {
-    // if (isMounted.current) {
-    //   const params = {
-    //     categoryId: categoryId > 0 ? categoryId : null,
-    //     sortProperty: sort.sortProperty,
-    //     currentPage,
-    //   };
-
-    //   const queryString = qs.stringify(params, { skipNulls: true });
-
-    //   navigate(`/?${queryString}`);
-    // }
-
-    // const params = qs.parse(window.location.search.substring(1)) as unknown as SearchPizzaParams;
-    // const sortObj = sortList.find((obj) => obj.sortProperty === params.sortBy);
-    // dispatch(
-    //   setFilters({
-    //     searchValue: params.search,
-    //     categoryId: Number(params.category),
-    //     currentPage: Number(params.currentPage),
-    //     sort: sortObj || sortList[0],
-    //   }),
-    // );
-
     getPizzas();
-    // isMounted.current = true;
   }, [categoryId, sort.sortProperty, searchValue, currentPage]);
-
-  // Парсим параметры при первом рендере
-  // React.useEffect(() => {
-  //   if (window.location.search) {
-  //     const params = qs.parse(window.location.search.substring(1)) as unknown as SearchPizzaParams;
-  //     const sort = sortList.find((obj) => obj.sortProperty === params.sortBy);
-  //     dispatch(
-  //       setFilters({
-  //         searchValue: params.search,
-  //         categoryId: Number(params.category),
-  //         currentPage: Number(params.currentPage),
-  //         sort: sort || sortList[0],
-  //       }),
-  //     );
-  //   }
-  //   isMounted.current = true;
-  // }, []);
 
   const pizzas = items.map((obj: any) => <PizzaBlock key={obj.id} {...obj} />);
   const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
